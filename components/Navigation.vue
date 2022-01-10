5e79a10a6d2269deec63acdeaffc1e4a99ef568d
Ganiyu Idris <golamide27@gmail.com>
2022-01-10 22:27:29 +0100

    Second commit

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
       <nav>
-          
+          <div class="nav-container py-8 px-5">
+              <div class="left-container">
+                  <div class="logo space-x-3">
+                      <img src="./svg/logo.svg" alt="Logo">
+
+                      <div class="text text-white font-medium">
+                          Hostel
+                      </div>
+                  </div>
+              </div>
+
+              <div class="right-container">
+                  <div class="avatar">
+
+                  </div>
+
+                  <button>
+
+                  </button>
+              </div>
+          </div>
       </nav>
   </div>
 </template>
